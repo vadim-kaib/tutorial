0e030fa71f865bd686100f31bec5d5218b1fc8e7
unknown <vadim.kaib.st@gmail.com>
2019-01-05 14:36:57 +0100

    add javapoint parameter reflection

---
+++ tutorial/src/javapoint/parameterReflection/Calculate.java
@@ -0,0 +1,11 @@
+package javapoint.parameterReflection;
+
+public class Calculate {  
+    int add(int a, int b){  
+        return (a+b);  
+    }  
+    int mul(int a, int b){  
+        return (b*a);  
+    }  
+}  
+
