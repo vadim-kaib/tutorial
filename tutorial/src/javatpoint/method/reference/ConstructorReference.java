86903234bbe1543a1e56167cb378e79fa889c682
unknown <vadim.kaib.st@gmail.com>
2020-01-28 22:46:42 +0100

    comment

--- tutorial/src/javatpoint/method/reference/ConstructorReference.java
+++ tutorial/src/javatpoint/method/reference/ConstructorReference.java
@@ -3,7 +3,7 @@ package javatpoint.method.reference;
 interface IConstructorReference {
 	ConstructorReference getMessage(String msg);
 }
-
+/*comment*/
 public class ConstructorReference {
 	ConstructorReference(String msg) {
 		System.out.print(msg);
