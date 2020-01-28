a44e406c650653f2dea94b87aa548049a6230bf9
unknown <vadim.kaib.st@gmail.com>
2020-01-28 22:47:41 +0100

    comment 2

--- tutorial/src/javatpoint/method/reference/InstanceMethodReference1.java
+++ tutorial/src/javatpoint/method/reference/InstanceMethodReference1.java
@@ -1,5 +1,6 @@
 package javatpoint.method.reference;
 
+/*comment2*/
 public class InstanceMethodReference1 {
 	public void saySomething() {
 		System.out.println("Hello, this is non-static method.");
