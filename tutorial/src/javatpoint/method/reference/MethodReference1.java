826e4b4453cba482b6001d733bc793f3ce1d43c4
unknown <vadim.kaib.st@gmail.com>
2020-01-28 23:11:26 +0100

    comments 4

--- tutorial/src/javatpoint/method/reference/MethodReference1.java
+++ tutorial/src/javatpoint/method/reference/MethodReference1.java
@@ -3,7 +3,9 @@ package javatpoint.method.reference;
 interface Sayable {
 	void say();
 }
-
+/*
+ * MethodReference1
+ * */
 public class MethodReference1 {
 	public static void saySomething() {
 		System.out.println("Hello, this is static method.");
--- tutorial/src/javatpoint/method/reference/MethodReference2.java
+++ tutorial/src/javatpoint/method/reference/MethodReference2.java
@@ -1,5 +1,7 @@
 package javatpoint.method.reference;
-
+/*
+ * MethodReference2
+ * */
 public class MethodReference2 {
     public static void ThreadStatus(){  
         System.out.println("Thread is running...");  
