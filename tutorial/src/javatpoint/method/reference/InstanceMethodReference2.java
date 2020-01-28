8040aeeecb26cbb78a9d805aa713a93e5b1b583a
unknown <vadim.kaib.st@gmail.com>
2020-01-28 22:58:47 +0100

    new comment

--- tutorial/src/javatpoint/method/reference/InstanceMethodReference2.java
+++ tutorial/src/javatpoint/method/reference/InstanceMethodReference2.java
@@ -1,5 +1,7 @@
 package javatpoint.method.reference;
-
+/*
+ * InstanceMethodReference2
+ * */
 public class InstanceMethodReference2 {
 	public void printnMsg() {
 		System.out.println("Hello, this is instance method");
